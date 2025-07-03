a9a36f33f4873460d52ec0a231c33e11cb912aef
KumarAnanta01 <Ananta.Kumar9096@gmail.com>
2025-07-03 10:31:26 +0530

    files are added

---
+++ src/main/java/org/example/FI/PredicatePersonDemo.java
@@ -0,0 +1,35 @@
+package org.example.FI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+class Person{
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;// initialize
+        this.age = age;// initialize
+    }
+
+}
+public class PredicatePersonDemo {
+    public static void main(String[] args) {
+        Person p1 = new Person("John", 26);
+        Person p2 = new Person("Smith", 16);
+        Person p3 = new Person("Raja", 36);
+        Person p4 = new Person("Rani", 6);
+
+        List<Person> persons = Arrays.asList(p1, p2, p3, p4);
+
+        Predicate<Person> predicate = p -> p.age >= 18;
+
+        for (Person person : persons) {
+            if (predicate.test(person)) {
+                System.out.println(person.name);
+            }
+
+        }
+    }
+}
